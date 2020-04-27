8b49a91799013a89829a77a3dd192d99e6e13768
Jack <Jackhmoss@gmail.com>
2020-04-27 22:39:53 +0100

    Modify port variable

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const port = process.env.PORT;
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
@@ -20,6 +21,6 @@ io.on('connection', function(socket) {
    });
 });
 
-http.listen(3000, function() {
-   console.log('listening on localhost:3000');
+http.listen(port, function() {
+   console.log('listening on localhost: ' + port);
 });
